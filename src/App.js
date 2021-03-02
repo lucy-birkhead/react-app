6356df608d944a28a4993bb9e60fd05f30fc4471
lucy-birkhead <76545912+lucy-birkhead@users.noreply.github.com>
2021-03-01 20:29:21 -0700

    Axios and API integration
    Altered App.js to call live info on page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./styles.css";
+
+
 export default function App() {
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+function handleResponse(response) {
+  console.log(response.data);
+  setWeatherData({
+    ready: true,
+    temperature: response.data.main.temp,
+    humidity: response.data.main.humidity,
+    date: "Wednesday 07:00",
+    description: response.data.weather[0].description,
+    iconUrl: "http://openweathermap.org/img/wn/10d@2x.png",
+    wind: response.data.wind.speed,
+    city: response.data.name
+  });
+  
+  setReady(true);
+}
+
+if (weatherData.ready) {
   return (
     <div className="weather-app-wrapper">
       <div className="container-1">
@@ -18,14 +40,16 @@ export default function App() {
           ></input>
         </form>
       </div>{" "}
-      <br /> <h1 id="city">Corrales</h1> <p> </p> <h2>Current Conditions:</h2>{" "}
-      <h4 id="description">
-        <em>Clear</em>
+      <br /> <h1 id="city">{weatherData.city}</h1> 
+      <br /> <p>{weatherData.date}</p> 
+      <h2>Current Conditions:</h2>{" "}
+      <h4 id="description" className="text-capitalize">
+        <em>{weatherData.description}</em>
       </h4>{" "}
       <div className="units">
         <h3>
-          <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="rain" id="icon" className="float-center" />
-          <div id="temperature">47</div>
+          <img src={weatherData.iconUrl} alt={weatherData.description} id="icon" className="float-center" />
+          <div id="temperature">{Math.round(weatherData.temperature)}</div>
           <a href="#" id="celsius-link" className="active">
             °C |
           </a>
@@ -34,10 +58,10 @@ export default function App() {
           </a>
           <ul className="standard-text">
             <li>
-              Humidity: <span id="humidity">30</span>%
+              Humidity: <span id="humidity">{weatherData.humidity}</span>%
             </li>
             <li>
-              Wind: <span id="wind">25</span> km/h
+              Wind: <span id="wind">{weatherData.wind}</span> km/h
             </li>
           </ul>
         </h3>
@@ -47,9 +71,20 @@ export default function App() {
         className="github"
         href="https://github.com/lucy-birkhead/react-app"
       >
-        Weather App Github Repository
+        React Weather App Github Repository
       </a>{" "}
       <p>Coded by Lucy B.</p>
     </div>
   );
+} else {
+  const apiKey = "19e635cfd49a09267430a421d97c1f42";
+  let city = "Corrales";
+  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+
+  return "Loading...";
+}
+
+
+ 
 }
