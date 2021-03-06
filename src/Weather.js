abbce6ab2f984d30fe31f7a5f2bbbe96a425f503
lucy-birkhead <76545912+lucy-birkhead@users.noreply.github.com>
2021-03-05 20:41:17 -0700

    Updated Icon URL

--- src/Weather.js
+++ src/Weather.js
@@ -17,7 +17,7 @@ function handleResponse(response) {
     humidity: response.data.main.humidity,
     date: new Date(response.data.dt * 1000),
     description: response.data.weather[0].description,
-    iconUrl: "http://openweathermap.org/img/wn/10d@2x.png",
+    iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}10d@2x.png`,
     wind: response.data.wind.speed,
     city: response.data.name
   });
