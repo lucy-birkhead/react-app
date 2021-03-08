2aea27c90dff15b77b1aaaaa4a453bc050d310cb
lucy-birkhead <76545912+lucy-birkhead@users.noreply.github.com>
2021-03-07 21:01:20 -0700

    Modified api url "https"

--- src/Weather.js
+++ src/Weather.js
@@ -29,7 +29,7 @@ function handleResponse(response) {
 function search() {
   const apiKey = "19e635cfd49a09267430a421d97c1f42";
   
-  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(handleResponse);
 }
 
